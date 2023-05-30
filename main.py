93d4fd1bce8a07699154c09fb8470af5ac7b3c81
hong <j50508@gmail.com>
2023-05-31 04:14:29 +0800

    test codes

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from pypdf import PdfReader
+# from pypdf import PdfReader
 
-reader = PdfReader("./src/yh_resume.pdf")
-number_of_pages = len(reader.pages)
-page = reader.pages[0]
-text = page.extract_text()
-print(text)
+# reader = PdfReader("./src/yh_resume.pdf")
+# number_of_pages = len(reader.pages)
+# page = reader.pages[0]
+# text = page.extract_text()
+# print(text)
+
+import pdfkit
+
+pdfkit.from_file('./src/yh_resume.pdf', 'example.html')
